749b23c1564a782b6bf791a6eafb91667fba105a
ivin jose <ivinnjose@gmail.com>
2022-12-16 23:35:31 +0530

    home page designing

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+
+	<!-- font awsome -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
+</head>
+<body>
+	<?php
+	include 'header.php';
+	?>
+
+	<div id="product_ads_1" class="space">
+		<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img src="./assets/images/m3.jpg" class="d-block w-100" alt="fff">
+				</div>
+				<div class="carousel-item">
+					<img src="./assets/images/m3.jpg" class="d-block w-100" alt="...">
+				</div>
+				<div class="carousel-item">
+					<img src="./assets/images/m3.jpg" class="d-block w-100" alt="...">
+				</div>
+			</div>
+			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Previous</span>
+			</button>
+			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Next</span>
+			</button>
+		</div>
+	</div>
+
+	<div id="product_ads_2">
+		<div class="row container-fluid" id="product_ads_2">
+			<div class="col-md-3 col-lg-3 container space">
+				<div class="img_box">
+					<a href="#"><img src="./assets/images/m3.jpg" class="d-block w-100" alt="..."></a>
+					<div class="container">
+						<h3>GT MST</h3>
+						<p>only $33</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-lg-3 container space">
+				<div class="img_box">
+					<a href="#"><img src="./assets/images/m3.jpg" class="d-block w-100" alt="..."></a>
+					<div class="container">
+						<h3>GT MST</h3>
+						<p>only $33</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-lg-3 container space">
+				<div class="img_box">
+					<a href="#"><img src="./assets/images/m3.jpg" class="d-block w-100" alt="..."></a>
+					<div class="container">
+						<h3>GT MST</h3>
+						<p>only $33</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-lg-3 container space">
+				<div class="img_box">
+					<a href="#"><img src="./assets/images/m1.jpg" class="d-block w-100" alt="..."></a>
+					<div class="container">
+						<h3>GT MST</h3>
+						<p>only $33</p>
+					</div>
+					
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<?php
+	include 'footer.php';
+	?>
+</body>
+</html>
